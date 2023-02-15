fb16aacafedd4a1312dbf502d63b3de9fdf2ed75
Fazekas Adrienn <adreeja17@gmail.com>
2023-02-15 16:23:41 +0100

    create the checkout field

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray)
+
 
 function getFeedHTML() {
     let feedHTML = ""
         menuArray.forEach( function(item) {
             feedHTML += `
-                    <div class="menu">
+                    <div class="container">
                         <ul class="menu-item" id="item-one">
                             <li class="emoji">${item.emoji}</li>
-                            <li>${item.name}</li>
-                            <li>${item.ingredients}</li>
-                            <li>${item.price}</li>
+                            <li class="food">${item.name}</li>
+                            <li class="ingredients">${item.ingredients}</li>
+                            <li class="price">${item.price}</li>
+                            <hr class="solid">
                             <button class="increase" id="increase">+</button>
-                        </ul>             
-                    </div>           
+                        </ul>
+                    </div> 
                 `
         })
         return feedHTML    
 }
-
 getFeedHTML()
+
 
 function render() {
     document.getElementById("feed").innerHTML = getFeedHTML()
