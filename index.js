2ea63b07e272fdb5a2ee5c311a62cc56a52e5421
Fazekas Adrienn <adreeja17@gmail.com>
2023-02-18 16:28:45 +0100

    find index of item in shoppinglist

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import { menuArray } from "./data.js";
 
 let checkoutList = document.getElementById("checkout-list")
 let checkout = document.getElementById("checkout")
+let moduleEl = document.getElementById("module")
+const consentForm = document.getElementById("consent-form")
 let shoppingList = []
 
 
 document.addEventListener("click", function (e) {
-
     if (e.target.dataset.increase) {
-        addOrdering(e.target.dataset.increase)
+        addOrder(e.target.dataset.increase)
     }
     else if (e.target.dataset.remove) {
         removeFoodItem(e.target.dataset.remove)
@@ -17,40 +18,57 @@ document.addEventListener("click", function (e) {
     else if (e.target.id == "purchase-btn") {
         completeOrder()
     }
+    else if (e.target.id == "pay-btn") {
+        payingOrder()
+    }
 })
 
-function completeOrder() {
-    const moduleEl = document.getElementById("module")
+function payingOrder() {
+    const formData = new FormData(consentForm)
+    console.log(formData)
+    const userName = formData.get("userName") 
+    moduleEl.style.display = "none"
+    checkout.innerHTML = ""
+
+    document.getElementById("finished-order").innerHTML = `
+        <div class="thankyou-text">Thanks, ${userName}! Your order  is on its way!</div>
+    `
+}
+
+function completeOrder() {    
     moduleEl.style.display = "block"
 }
 
 function removeFoodItem(index) {
-
+    shoppingList.splice(index, 1)  
+    
 }
 
 
-function addOrdering(itemId) {
+function addOrder(itemId) {
     let targetMenuArray = menuArray[itemId]
     
     checkout.style.display = "block"
     
+    /* shopping lista amiben tároljuk az árakat majd összegezzük őket is kiiratjuk */
+    shoppingList.push(targetMenuArray)
+    let index = shoppingList.lastIndexOf(targetMenuArray)
+
     checkoutList.innerHTML += `
-            <div class="checkout-item" id="checkout-item"> 
+            <div class="checkout-item" data-remove="${index}" id="checkout-item"> 
                 ${targetMenuArray.name}
             </div> 
-            <button class="remove-item-btn" data-remove="${targetMenuArray.id}">remove</button>
-            <div class="checkout-price" id="checkout-price">    
+            <button class="remove-item-btn" data-remove="${index}">remove</button>
+            <div class="checkout-price" data-remove="${index} id="checkout-price">    
                 $${targetMenuArray.price} 
             </div>
             <div class="line-break"></div>
         `
-    /* shopping lista amiben tároljuk az árakat majd összegezzük őket is kiiratjuk */
-    shoppingList.push(targetMenuArray.price)
-    
+  
     if (shoppingList) {
         let sum = 0
-        shoppingList.forEach( function (price) {
-                sum += price
+        shoppingList.forEach( function (item) {
+                sum += item.price
         })
         document.getElementById("checkout-totel-price").innerHTML = `
             <h2 class="checkout-totel-price" id="checkout-totel-price">
@@ -58,12 +76,7 @@ function addOrdering(itemId) {
             </h2>
         `
         }
-    render()
 }
-
-/* let sum = 0;
-shoppingCartArray.forEach(item => sum += item.price);
-*/
 
 function getFeedHTML() {
     let feedHTML = ""
@@ -84,7 +97,6 @@ function getFeedHTML() {
         return feedHTML    
 }
 getFeedHTML()
-
 
 function render() {
     document.getElementById("feed").innerHTML = getFeedHTML()
